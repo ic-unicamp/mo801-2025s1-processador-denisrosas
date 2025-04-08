0f2fb6ef69845168fc2a51ff08dda3ffedd959c5
Denis Rosas <denis.rosas@gmail.com>
2025-04-08 17:00:54 -0300

    corringindo o bit de controle do extend

--- extend.v
+++ extend.v
@@ -1,6 +1,6 @@
 module extend (
-    input [31:7] instr,       // Instruction bits 31:7
-    input [2:0] imm_src,      // Immediate format selector
+    input [31:0] instr,       // Instruction bits 31:0
+    input [1:0] imm_src,      // Immediate format selector
                               // 000: R-type (no immediate)
                               // 001: I-type
                               // 010: S-type
@@ -13,13 +13,13 @@ module extend (
 always @(*) begin
     case (imm_src)
         // I-type immediate (12 bits: instr[31:20])
-        3'b001: imm_ext = {{20{instr[31]}}, instr[31:20]};
-        
-        // S-type immediate (12 bits: {instr[31:25], instr[11:7]})
-        3'b010: imm_ext = { {20{instr[31]} }, instr[31:25], instr[11:7]};
+        3'b00: imm_ext = {{20{instr[31]}}, instr[31:20]};
         
         // B-type immediate (13 bits: {instr[31], instr[7], instr[30:25], instr[11:8], 1'b0})
-        3'b011: imm_ext = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
+        3'b01: imm_ext = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
+        
+        // S-type immediate (12 bits: {instr[31:25], instr[11:7]})
+        3'b10: imm_ext = { {20{instr[31]} }, instr[31:25], instr[11:7]};
         
         // R-type has no immediate (output 0)
         default: imm_ext = 32'b0;  // Default case covers R-type and undefined codes

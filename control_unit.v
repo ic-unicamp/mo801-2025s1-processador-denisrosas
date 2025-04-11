5cf251b9399c43f3028eb49cac833e39ba079db7
Denis Rosas <denis.rosas@gmail.com>
2025-04-10 23:31:47 -0300

    Atualizando a control unit para suportar somente a instrução addi.  - limitei os possiveis states - dei nome para todos os valores enviados pela control unit - criei o state  PC_PLUS_4

--- control_unit.v
+++ control_unit.v
@@ -21,21 +21,47 @@ module control_unit(
     reg [2:0] state, next_state;
 
     // Estados da máquina de estados
-    localparam FETCH = 3'b000;
-    localparam DECODE = 3'b001;
-    localparam EXECUTE_L = 3'b010;
-    localparam EXECUTE_S = 3'b011;
-    localparam EXECUTE_B = 3'b010;
-    localparam MEMORY_ACCESS = 3'b100;
-    localparam WRITEBACK = 3'b101;
-
-//    state_t state, next_state;
+    localparam FETCH = 0;
+    localparam DECODE = 1;
+    localparam EXECUTE = 2;
+    localparam MEMORY_ACCESS = 3;
+    localparam WRITEBACK = 4;
+    localparam PC_PLUS_4 = 5;
 
     // Definição de opcodes e funct3
+    localparam OPCODE_ITYPE = 7'b0010011;
     localparam OPCODE_LTYPE = 7'b0000011;
     localparam OPCODE_STYPE = 7'b0100011;
     localparam OPCODE_RTYPE = 7'b0110011;
     localparam OPCODE_BTYPE = 7'b1100011;
+
+    //imm source types
+    localparam IMMSRC_ITYPE = 2'b00;
+    localparam IMMSRC_STYPE = 2'b01;
+    localparam IMMSRC_BTYPE = 2'b10;
+
+    //alu A source
+    localparam ALUSRCA_PC = 2'b00;
+    localparam ALUSRCA_OLDPC = 2'b01;
+    localparam ALUSRCA_RD1 = 2'b10;
+
+    //alu B source
+    localparam ALUSRCB_RD2 = 2'b00;
+    localparam ALUSRCB_IMMEXT = 2'b01;
+    localparam ALUSRCB_4 = 2'b10;
+
+    // alu control
+    localparam ALUCTRL_AND  = 3'b000;
+    localparam ALUCTRL_OR   = 3'b001;
+    localparam ALUCTRL_ADD  = 3'b010;
+    localparam ALUCTRL_SUB  = 3'b110;
+    localparam ALUCTRL_SLT  = 3'b111;
+
+    // Result source
+    localparam RESSRC_ALUOUT = 2'b10;
+    localparam RESSRC_MEM = 2'b01;
+    localparam RESSRC_PC4 = 2'b00;
+    localparam RESSRC_ZERO = 2'b11;
 
     // Funct3 valores para instruções R-type
     localparam FUNCT3_ADD_SUB = 3'b000;
@@ -70,54 +96,52 @@ module control_unit(
 
         case (state)
             FETCH: begin
-                irwrite = 1;
+                adrsource = 0;
                 next_state = DECODE;
             end
 
+            //mais um ciclo pra dar tempo de armazenar a instrucao no 
             DECODE: begin
+                irwrite = 1;
+                next_state = EXECUTE;
+            end
+
+            EXECUTE: begin
                 case (opcode)
-                    OPCODE_LTYPE: begin
-                                  imm_source = 2'b00;
-                                  next_state = EXECUTE_L;
+                    OPCODE_ITYPE: begin
+                                  imm_source = IMMSRC_ITYPE;
+                                  alu_source_a = ALUSRCA_RD1;
+                                  alu_source_b = ALUSRCB_IMMEXT;
+                                  alu_control = 3'b000; //add
+                                  next_state = WRITEBACK;
                     end
-                    OPCODE_STYPE: begin
-                                  imm_source = 2'b01;
-                                  next_state = EXECUTE_S;
-                    end
-                    // OPCODE_RTYPE: next_state = EXECUTE_R;
-                    // OPCODE_BTYPE: next_state = EXECUTE_B;
                     default: next_state = FETCH;
                 endcase
             end
 
-            EXECUTE_L: begin
-                alu_source_a = 2'b00; //read from rd1
-                alu_source_a = 2'b01; //read from extend
-                alu_control  = 3'b010; //srca + srcb
-                next_state = MEMORY_ACCESS;
-            end
-
-            EXECUTE_S: begin
-                alu_source_a = 2'b00; //read from rd1
-                alu_source_a = 2'b01; //read from extend
-                alu_control  = 3'b010; //srca + srcb
-                next_state = MEMORY_ACCESS;
-            end
-
-            MEMORY_ACCESS: begin
-                case (opcode)
-                    OPCODE_LTYPE: begin
-                                  adrsource = 1;
-                                  next_state = WRITEBACK;
-                    end
-                    OPCODE_STYPE: next_state = EXECUTE_S;
-                default: next_state = FETCH;
-                endcase
-            end
+            // MEMORY_ACCESS: begin
+            //     case (opcode)
+            //         OPCODE_LTYPE: begin
+            //                       adrsource = 1;
+            //                       next_state = WRITEBACK;
+            //         end
+            //         OPCODE_STYPE: next_state = EXECUTE_S;
+            //     default: next_state = FETCH;
+            //     endcase
+            // end
 
             WRITEBACK: begin
                 regwrite = 1'b1;
-                resultsource = 2'b01;
+                resultsource = RESSRC_ALUOUT;
+                next_state = PC_PLUS_4;
+            end
+
+            PC_PLUS_4: begin
+                alu_source_a = ALUSRCA_PC;
+                alu_source_b = ALUSRCB_4;
+                alu_control = ALUCTRL_ADD;
+                resultsource = RESSRC_PC4;
+                pcwrite = 1'b1;
                 next_state = FETCH;
             end
 
